aa384a671452fa1ccf5fd05afc0f4bbda1a70d5d
tgraha09 <tfire09@gmail.com>
2020-10-06 10:24:02 -0400

    Added in my assets, will continue merging changes.

---
+++ Team Riot Project 2/Assets/Scripts/RuneMatch/ScrollTool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTool : MonoBehaviour
+{
+    public GameObject scroll;
+    public GameObject[] runes = new GameObject[26];
+    private GameObject openscroll = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
