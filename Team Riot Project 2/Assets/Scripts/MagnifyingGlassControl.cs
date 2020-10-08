1c0a1948171fb3b77405f201c74eeccf0272259a
QI JUN HE <heqijun130@gmail.com>
2020-10-08 12:44:30 -0400

    main scene finished
    main scene finished

---
+++ Team Riot Project 2/Assets/Scripts/MagnifyingGlassControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnifyingGlassControl : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    // Update is called once per frame
+    public void PointerEnter()
+    {
+        transform.position += Vector3.up * 40.0f;
+    }
+
+    public void PointerExit()
+    {
+        transform.position += Vector3.down * 40.0f;
+    }
+
+    public void Click()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+}
