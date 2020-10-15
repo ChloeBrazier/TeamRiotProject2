409b8db5032dddb9d28276835b48df401e71fbc0
QI JUN HE <heqijun130@gmail.com>
2020-10-14 20:45:37 -0400

    IntroScene
    IntroScene

---
+++ Team Riot Project 2/Assets/Scripts/IntroClose.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroClose : MonoBehaviour
+{
+
+    public GameObject introBox;
+
+    public void Clicked()
+    {
+        Destroy(gameObject);
+        Destroy(introBox);
+    }
+
+}
